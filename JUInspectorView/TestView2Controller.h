fbb0752ad5e2cd18c4c8e1eaa1b8b47ccfd33b0d
Chris Hill <chris@enemyhideout.com>
2011-08-23 14:03:59 -0700

    Adding test view controller to name the subviews.

---
+++ JUInspectorView/TestView2Controller.h
@@ -0,0 +1,17 @@
+//
+//  TestView2Controller.h
+//  JUInspectorView
+//
+//  Created by Chris Hill on 8/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TestView2Controller : NSViewController
+{
+	@public
+	IBOutlet NSTextField* textField;
+}
+
+@end
---
+++ JUInspectorView/TestView2Controller.m
@@ -0,0 +1,23 @@
+//
+//  TestView2Controller.m
+//  JUInspectorView
+//
+//  Created by Chris Hill on 8/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestView2Controller.h"
+
+@implementation TestView2Controller
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
